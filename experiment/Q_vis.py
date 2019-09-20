6a4e7902480251c6d924aa0bdfa189a4f6162c72
Qizhu Huang <qizhu.huang@franka.de>
2019-09-20 16:50:25 +0200

    Fix visualization bug

--- experiment/Q_vis.py
+++ experiment/Q_vis.py
@@ -13,12 +13,10 @@ from abstract_gym.scenario.scene_0 import Scene
 def sample_max_q_value(q_net, state, sample_size=500, scale_factor=0.1, batch_size=1):
     action_list = (np.random.rand(sample_size * batch_size, 2) - 0.5) * scale_factor
     action_list = torch.Tensor(action_list)
-    print("state", state)
     x = torch.repeat_interleave(state, sample_size)
     x = x.reshape(batch_size, -1, sample_size)
     x = torch.transpose(x, 2, 1)
     state_list = x.reshape(-1, 2).to(dtype=torch.float)
-    print(state_list)
     input_x = torch.cat((state_list, action_list), 1)
     input_x = input_x.to(device, dtype=torch.float)
     with torch.no_grad():
@@ -27,10 +25,8 @@ def sample_max_q_value(q_net, state, sample_size=500, scale_factor=0.1, batch_si
     max_q, _ = torch.max(q_list, 1)
     return max_q
 
-if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    Q = Q_net2().to(device)
-    Q.load_state_dict(torch.load("../models/q_net2_v2_62029_53.pth"))
+def Q_visualizer(q_net, model_file, batch_size=30000, batch_num=100):
+    q_net.load_state_dict(torch.load(model_file))
     occ = OccupancyGrid(random_obstacle=False)
     scene = Scene(env=occ, visualize=False)
     q_list = []
@@ -38,14 +34,12 @@ if __name__ == "__main__":
     y_list = []
     j1_list = []
     j2_list = []
-    batch_size = 2
-    for i in range(np.int64(1)):
+    for i in range(np.int64(batch_num)):
         xb_list = []
         yb_list = []
         j1b_list = []
         j2b_list = []
-        qb_list = []
-        print("i ", i)
+        print("i :", i)
         for k in range(batch_size):
             scene.random_valid_pose()
             j1 = scene.robot.joint_1
@@ -55,8 +49,8 @@ if __name__ == "__main__":
             yb_list.append(EE.y)
             j1b_list.append(j1)
             j2b_list.append(j2)
-        state = torch.Tensor([np.squeeze(j1b_list), np.squeeze(j2b_list)])
-        q_value = sample_max_q_value(Q, state, batch_size=batch_size)
+        state = torch.transpose(torch.Tensor([np.squeeze(j1b_list), np.squeeze(j2b_list)]), 0, 1)
+        q_value = sample_max_q_value(q_net, state, batch_size=batch_size)
         q_value = q_value.to(torch.device("cpu")).numpy()
         q_list += q_value.tolist()
         j1_list += j1b_list
@@ -64,10 +58,22 @@ if __name__ == "__main__":
         x_list += xb_list
         y_list += yb_list
 
-    area = 10
-    print(q_list)
+    area = 0.1
     print("q_list min max :", np.array(q_list).min(), np.array(q_list).max())
-    offset = np.array(q_list) - np.abs(np.array(q_list).min())
+    if np.array(q_list).min() < 0:
+        offset = np.array(q_list) + np.abs(np.array(q_list).min())
+    else:
+        offset = np.array(q_list) - np.abs(np.array(q_list).min())
     fracs = offset.astype(float) / offset.max()
+    plt.figure()
+    plt.subplot(121)
+    plt.scatter(x_list, y_list, s=area, c=fracs)
+    plt.subplot(122)
     plt.scatter(j1_list, j2_list, s=area, c=fracs)
     plt.show()
+
+if __name__ == "__main__":
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    Q = Q_net3().to(device)
+    model_file = "../models/q_net3_v1_17688_167.pth"
+    q_vis = Q_visualizer(Q, model_file,3000, 10)
